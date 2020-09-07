45d8173891614fd4dea219055b28d08a146f747c
Waleed A. Afifi <waleedafifich@gmail.com>
2020-09-08 00:44:30 +0300

    Challenge 2 solution

---
+++ challenges/arrayShift/README.md
@@ -0,0 +1,13 @@
+# data-structures-and-algorithms
+
+# ArrayShift
+This challenge is about insrt a value in the middel of an array
+
+## Challenge
+Insert and shift an array in middle at index
+
+## Approach & Efficiency
+Learn how to insert value in the middel on an array without using built in functino.
+
+## Solution
+![](./assets/array-shift.jpg)
---
+++ challenges/arrayShift/array-shift.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const insertShiftArray = (arr, num) => {
+    let ln = Math.ceil(arr.length / 2);
+    let copy = [];
+    for (let i = 0; i < arr.length + 1; i++) {
+        if (i === ln) {
+            copy[i] = num;
+        } else if (i > ln) {
+            copy[i] = arr[i - 1];
+        } else {
+            copy[i] = arr[i];
+        }
+    }
+    return copy;
+};
+
+  console.log(insertShiftArray([4,8,15,23,42],16));
+  console.log(insertShiftArray([2,4,6,8], 5));
+
+module.exports =insertShiftArray;
