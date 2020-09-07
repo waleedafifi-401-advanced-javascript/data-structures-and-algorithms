49d52dba5fcc7bb6f7496e114cae83ff90035090
Waleed A. Afifi <57403758+waleedafifi90@users.noreply.github.com>
2020-09-08 01:28:06 +0300

    Update array-reverse.js

--- challenges/arrayReverse/array-reverse.js
+++ challenges/arrayReverse/array-reverse.js
@@ -1,18 +1,23 @@
-'use strict'
+'use strict';
 
-let arr = [89, 2354, 3546, 23, 10, -923, 823, -12];
-let revarsedArray = [];
+let testArr = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199];
 
-function reverseArray(arr) {
-    arr.forEach((element, i) => {
-        revarsedArray.push(arr[arr.length - 1 - i]);
-        return revarsedArray;
-    });
+const reverseArray = (arr) => {
+
+    let ln = arr.length - 1;
+
+    for (let i = 0; i < ln; i++) {
+        let changeNum = arr[i];
+        arr[i] = arr[ln];
+        arr[ln] = changeNum;
+        ln--;
+    }
+    console.log(arr)
+    return arr
 };
-reverseArray(arr)
-console.log(revarsedArray);
 
 
+reverseArray(testArr);
 
 //Strech goals
 // function reverseArray(arr) {
