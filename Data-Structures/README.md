89fba98f467575b9fec6ce1e01a3f8f016149a29
Waleed A. Afifi <waleedafifich@gmail.com>
2020-09-15 00:54:29 +0300

    lab 7 done

--- Data-Structures/README.md
+++ Data-Structures/README.md
@@ -5,22 +5,29 @@
     - Insert method takes any value as an argument and adds a new node with that value to the head
     - Include method which takes any value as an argument and returns a boolean result depending on whether that value exists as a Node’s value somewhere within the list.
     - toString method takes in no arguments and returns a string representing all the values in the Linked List, formatted as: `"{ a } -> { b } -> { c } -> NULL"`
+
     - append(value) which adds a new node with the given value to the end of the list
     - insertBefore(value, newVal) which add a new node with the given newValue immediately before the first value node
     - insertAfter(value, newVal) which add a new node with the given newValue immediately after the first value node
+
+    - kthFromEnd(k) Return the node’s value that is k from the end of the linked list.
 
 ## Challenge
 Define three method 
 - insert method take a value as argument and add it to a newe Node
 - include method take a value and return a boolean wether its exists as a node or not
 - toString method return all values in the linked list as string.
+
 - append method take a value and add the new value at the end of list
-- insertBefore method take two value the first is the seaarch value and the second is the newe value and then add thee nwew value before the value
-- insretAfter method take two value the first is the seaarch value and the second is the newe value and then add thee nwew value after the value
+- insertBefore method take two value the first is the search value and the second is the newe value and then add thee nwew value before the value
+- insretAfter method take two value the first is the search value and the second is the newe value and then add thee nwew value after the value
+
+- kthFromEnd method take numbre and the method will return the value of that index.
 
 ## Approach & Efficiency
 - create a **linked list** have a methods to add nodes.
 - create another method to search in the **linked list** using while loop.
 - Insertion method, at last, aftre value, and before value
+- Search method from the end of the **linked list** end take a `k` as a variable, and it rreturn the value of that index, this method will check if `k` grater/smaller than list length, and must be a number 
 
-![](../assets/extend-linked-list.jpg)
+![](../assets/kthFromEnd.jpg)
--- __test__/linked-list.test.js
+++ __test__/linked-list.test.js
@@ -44,25 +44,25 @@ describe('Linked List Module', () => {
         linkedList.insertBefore('Flaming Katy', 'Please');
         expect(linkedList.head.next.next.next.name).toBe('Please');
     })
-    
+
     it('Will insert after something in the middle', () => {
         const linkedList = cactusList();
         linkedList.insertAfter('Ball Cactus', 'Bunny Cactus');
         expect(linkedList.head.next.next.next.name).toBe('Bunny Cactus');
     })
-    
+
     it('Will insert after something at the end', () => {
         const linkedList = cactusList();
         linkedList.insertAfter('Flaming Katy', 'Tiger Tooth Aloe');
         expect(linkedList.head.next.next.next.next.name).toBe('Tiger Tooth Aloe');
     })
-    
+
     it('Will append to the end of the list', () => {
         const linkedList = cactusList();
         linkedList.append('Tiger Tooth Aloe');
         expect(linkedList.head.next.next.next.next.name).toBe('Tiger Tooth Aloe');
     })
-    
+
     it('Will apend multiple items to the end of the list', () => {
         const linkedList = cactusList();
         linkedList.append('Ariocarpus Iloydii');
@@ -71,7 +71,36 @@ describe('Linked List Module', () => {
         expect(linkedList.include('Medusita')).toBeTruthy;
         expect(linkedList.head.next.next.next.next.name).toEqual('Ariocarpus Iloydii');
         expect(linkedList.head.next.next.next.next.next.name).toEqual('Medusita');
-      })
+    })
+});
+
+describe('Linked List kth from the end', () => {
+
+    it('throws an error when k is greater than list length', () => {
+        const linkedList = cactusList();
+        expect(() => linkedList.kthFromEnd(5)).toThrowError('The value you have entered out of the linked list range');
+    });
+
+    it('works when k points to the first node', () => {
+        const linkedList = cactusList();
+        expect(linkedList.kthFromEnd(3)).toBe('Moon cactus');
+    });
+
+    it('throws an error when k is negative', () => {
+        const linkedList = cactusList();
+        expect(() => linkedList.kthFromEnd(-3)).toThrowError('Value can not be less than 0');
+    });
+
+    it('works if the list is only one node long', () => {
+        const linkedList = new LinkedList();
+        linkedList.insert('Shafer Opuntia');
+        expect(linkedList.kthFromEnd(0)).toBe('Shafer Opuntia');
+    });
+
+    it('works on the happy path', () => {
+        const linkedList = cactusList();
+        expect(linkedList.kthFromEnd(2)).toBe('Aloe Vera');
+    });
 });
 
 const cactusList = () => {
