3112dd690cf99350bda15c541703e0b717422166
Waleed A. Afifi <waleedafifich@gmail.com>
2020-10-20 01:03:58 +0300

    CC 33

--- challenges/repeatedWord/repeated-word.js
+++ challenges/repeatedWord/repeated-word.js
@@ -27,12 +27,14 @@ const repeatedWord = (str) => {
     if (counter > 1 && splitedWord[i] !== '0') {
       // return the word array oof i
       return splitedWord[i];
+    } else {
+      return 'All words are unique ^_^';
     }
   }
 };
 
-// let str = 'It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of Darkness, it was the spring of hope, it was the winter of despair, we had everything before us, we had nothing before us, we were all going direct to Heaven, we were all going direct the other way – in short, the period was so far like the present period, that some of its noisiest authorities insisted on its being received, for good or for evil, in the superlative degree of comparison only.';
-// let word = repeatedWord(str);
-// console.log(word);
+let str = 'It was the';
+let word = repeatedWord(str);
+console.log(word);
 
 module.exports = repeatedWord;
---
+++ challenges/treeIntersection/readme.md
@@ -0,0 +1,13 @@
+## Tree Intersection
+
+### Challenge
+- Write a function called tree_intersection that takes two binary tree parameters. Without utilizing any of the built-in library methods, and return a set of values found in both trees.
+
+### Approach & Efficiency
+- I used function, for loop, if statements, and while loop.
+
+### Big O:
+Time: O(n), Space: O(1)
+
+### Solution
+![Tree Intersection](../../assets/tree-intersection.jpg)
